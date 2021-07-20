ea5cce69c87e6b9f4469d0b09d811fa6dc290fe7
vuongvgc <vuongvgc@gmail.com>
2021-07-20 20:39:16 +0700

    change image of project

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import TopNav from "../components/TopNav";
-import Header from "../components/Header";
+import Header from "../layouts/Header";
 import FlashDeals from "../components/FlashDeals";
 import HowDoesItWork from "../components/HowDoesItWork";
 import PopularMeals from "../components/PopularMeals";
@@ -9,7 +9,7 @@ import FeaturesFood from "../components/FeaturesFood";
 import AppDownload from "../components/AppDownload";
 import DetailsFood from "../components/DetailsFood";
 import CTA from "../components/CTA";
-import Footer from "../components/Footer";
+import Footer from "../layouts/Footer";
 const HomePage: React.FC = () => {
   return (
     <div className="container">
