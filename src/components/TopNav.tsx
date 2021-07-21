6c5cd803db32a4c676ce26511dd1f40349c4298e
vuongvgc <vuongvgc@gmail.com>
2021-07-21 22:18:47 +0700

    fix navbar: fix html tag

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -3,7 +3,7 @@ const TopNav: React.FC = () => {
     <nav className="row nav-bar__box">
       <div className="col-1-of-4 ">
         <div className="logo_box">
-          <img src="./img/logo.png" alt="food Wagon" />
+          <img src="./img/logo.png" alt="Logo food Wagon" />
         </div>
       </div>
       <div className="col-2-of-4 nav-bar__location">
@@ -22,27 +22,23 @@ const TopNav: React.FC = () => {
       <div className="col-1-of-4 row nav-bar__button">
         <div>
           <p className="nav-search row">
-            <div>
-              <img
-                className="icon-small"
-                src="./img/icon/search.png"
-                alt="search"
-              />
-            </div>
-            <div>Search Food</div>
+            <img
+              className="icon-small"
+              src="./img/icon/search.png"
+              alt="search"
+            />
+            Search Food
           </p>
         </div>
         <div>
           <button className="button-login">
             <a className="row" href="/">
-              <div>
-                <img
-                  className="icon-small"
-                  src="./img/icon/user.png"
-                  alt="search"
-                />
-              </div>
-              <div>Login</div>
+              <img
+                className="icon-small"
+                src="./img/icon/user.png"
+                alt="search"
+              />
+              Login
             </a>
           </button>
         </div>
