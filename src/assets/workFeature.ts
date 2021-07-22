c4e55408690927f9832718d1f3e6307dc7ef97aa
vuongvgc <vuongvgc@gmail.com>
2021-07-22 15:54:33 +0700

    build how does it work components

---
+++ src/assets/workFeature.ts
@@ -0,0 +1,27 @@
+export const workFeature = [
+  {
+    id: 1,
+    icon: "./img/icon/select-location.png",
+    title: "Select location",
+    description: "Choose the location where your food will be delivered.",
+  },
+  {
+    id: 2,
+    icon: "./img/icon/order.png",
+    title: "Choose order",
+    description: "Check over hundreds of menus to pick your favorite food",
+  },
+  {
+    id: 3,
+    icon: "./img/icon/pay-advanced.png",
+    title: "Select location",
+    description:
+      "It's quick, safe, and simple. Select several methods of payment",
+  },
+  {
+    id: 4,
+    icon: "./img/icon/enjoy-meals.png",
+    title: "Enjoy meals",
+    description: "Food is made and delivered directly to your home.",
+  },
+];
--- src/layouts/HowDoesItWork.tsx
+++ src/layouts/HowDoesItWork.tsx
@@ -1,12 +1,17 @@
+import WorkFeatureItem from "../components/WorkFeatureItem";
+import { workFeature } from "../assets/workFeature";
 const HowDoesItWork: React.FC = () => {
   return (
     <div className="how-it-work__box">
       <h2 className="header-secondary">How does it work</h2>
-      <div className="row Work-features__box border-test">
-        <div className="col-1-of-4 border-test">Item 1</div>
-        <div className="col-1-of-4 border-test">Item 2</div>
-        <div className="col-1-of-4 border-test">Item 3</div>
-        <div className="col-1-of-4 border-test">Item 4</div>
+      <div className="row Work-features__box">
+        {workFeature.map((feature) => {
+          return (
+            <div className="col-1-of-4">
+              <WorkFeatureItem feature={feature} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
