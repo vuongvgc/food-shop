4bf9630f43ea39afab95dd174849aa810a2db9fc
vuongvgc <vuongvgc@gmail.com>
2021-07-25 03:18:46 +0700

    build banner

--- src/layouts/CTA.tsx
+++ src/layouts/CTA.tsx
@@ -1,4 +1,17 @@
 const CTA: React.FC = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="container-fluid cta__box">
+      <img className="cta__banner" src="./img/banner.png" alt="banner" />
+      <div className="cta__content">
+        <h2>Are you ready to order with the best deals?</h2>
+        <button>
+          <a href="/">
+            Proceed to order
+            <img src="./img/icon/next.png" alt="next" />
+          </a>
+        </button>
+      </div>
+    </div>
+  );
 };
 export default CTA;
